af9ccaf65f96ecbd347b8fc8938f048fc262281a
Joe Groocock <me@frebib.net>
2024-07-13 23:18:25 +0000

    Use exporter-toolkit/web for TLS support
    Signed-off-by: Joe Groocock <me@frebib.net>

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"log"
 	"net/http"
 
+	kitlog "github.com/go-kit/log"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/exporter-toolkit/web"
 
 	"github.com/frebib/zfs-exporter/collector"
 	"github.com/frebib/zfs-exporter/zfs"
@@ -17,6 +19,7 @@ import (
 var (
 	listenAddress = flag.String("web.listen-address", ":9254", "Address on which to expose metrics and web interface.")
 	metricsPath   = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+	webConfigFile = flag.String("web.config.file", "", "Path to web-config file")
 )
 
 func main() {
@@ -39,17 +42,27 @@ func main() {
 	registry.MustRegister(collector.NewZpoolCollector(libzfs))
 	registry.MustRegister(collector.NewDatasetCollector(libzfs))
 
-	http.Handle(*metricsPath, promhttp.HandlerFor(registry, opts))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := http.NewServeMux()
+	router.Handle(*metricsPath, promhttp.HandlerFor(registry, opts))
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, `<html><head><title>ZFS Exporter</title></head>`+
 			`<body><h1>ZFS Exporter</h1>`+
 			`<p><a href="%s">Metrics</a></p></body></html>`,
 			*metricsPath,
 		)
 	})
+	server := &http.Server{
+		Addr:    *listenAddress,
+		Handler: router,
+	}
 
-	log.Printf("Listening on %s", *listenAddress)
-	err = http.ListenAndServe(*listenAddress, nil)
+	logger := kitlog.NewLogfmtLogger(log.Writer())
+	config := web.FlagConfig{
+		WebListenAddresses: &[]string{*listenAddress},
+		WebConfigFile:      webConfigFile,
+	}
+
+	err = web.ListenAndServe(server, &config, logger)
 	if !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("%s", err)
 	}
